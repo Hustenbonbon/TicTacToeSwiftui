dbea27a7aeccabfab1765276d90634e3c5072299
Max Tharr <max.tharr@mayflower.de>
2020-05-19 18:41:57 +0200

    Initial Commit

---
+++ TicTacToeDemo/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  TicTacToeDemo
+//
+//  Created by Max Tharr on 19.05.20.
+//  Copyright © 2020 Mayflower GmbH. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
